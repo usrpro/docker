9d6e150046480f0a8b8c983d4301c1441528cd7f
Ionut Filip <ionut.philip@gmail.com>
2019-01-14 14:51:08 +0200

    Added imap dependencies and fixed config file

--- 14.0/apache/config/imap.config.php
+++ 14.0/apache/config/imap.config.php
@@ -1,10 +1,12 @@
 <?php
+$CONFIG = array (
 
-'user_backends' => array(
-    array(
-        'class' => 'OC_User_IMAP',
-        'arguments' => array(
-            '{imap.example.com:993/imap/ssl}', 'example.com'
-        ),
-    ),
-)
+  'user_backends' => array(
+      array(
+          'class' => 'OC_User_IMAP',
+          'arguments' => array(
+              '{imap.example.com:993/imap/ssl}', 'example.com'
+          ),
+      ),
+  )
+);
